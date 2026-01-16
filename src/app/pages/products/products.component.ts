23da30cdb85c89861a69893cc08e84ff74d82266
Shashith-Malshan <malshanshashith318@gmail.com>
2026-01-16 17:29:28 +0530

    Add ProductCard component and update Products page layout

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [NavbarComponent,ProductCardComponent,FooterComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })

8d1520b171e32966313b88c634096581560c375b
Shashith-Malshan <malshanshashith318@gmail.com>
2026-01-16 17:40:38 +0530

    Refactor ProductsService to include HttpClient for API calls

--- src/app/api/products.service.ts
+++ src/app/api/products.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private productServiceAPI: HttpClient) { }
+
+  getProducts(){
+    return this.productServiceAPI.get<any>("https://dummyjson.com/products");
+  }
 }
